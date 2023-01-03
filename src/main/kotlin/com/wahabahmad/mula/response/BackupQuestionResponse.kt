b96589334dda76c26376364f7e8f5f92aa6b9d8d
Wahab Ahmad <w28ahmad@uwaterloo.ca>
2023-01-02 22:00:47 -0500

    Fixes for question wrong penalty

--- src/main/kotlin/com/wahabahmad/mula/response/BackupQuestionResponse.kt
+++
@@ -1,9 +0,0 @@
-package com.wahabahmad.mula.response
-
-import com.wahabahmad.mula.data.SocketMessageTypes
-import com.wahabahmad.mula.model.Question
-
-data class BackupQuestionResponse(
-    val type: SocketMessageTypes = SocketMessageTypes.BACKUP_QUESTION,
-    val backupQuestion: Question
-)
