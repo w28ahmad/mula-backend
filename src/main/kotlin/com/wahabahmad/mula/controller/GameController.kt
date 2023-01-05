bf417c87b2471945300a1b29ec2efb4271da1be9
Wahab Ahmad <w28ahmad@uwaterloo.ca>
2023-01-05 17:35:41 -0500

    Updating debug sessionId endpoint

--- src/main/kotlin/com/wahabahmad/mula/controller/GameController.kt
+++ src/main/kotlin/com/wahabahmad/mula/controller/GameController.kt
@@ -4,12 +4,10 @@ import com.wahabahmad.mula.data.SocketMessageTypes
 import com.wahabahmad.mula.request.BeginGameDebugRequest
 import com.wahabahmad.mula.request.BeginGameRequest
 import com.wahabahmad.mula.request.QuestionSolutionRequest
-import com.wahabahmad.mula.response.QuestionSetResponse
 import com.wahabahmad.mula.response.QuestionSolutionResponse
 import com.wahabahmad.mula.service.GameService
 import com.wahabahmad.mula.service.QuestionService
 import org.springframework.messaging.handler.annotation.MessageMapping
-import org.springframework.messaging.handler.annotation.SendTo
 import org.springframework.messaging.simp.SimpMessagingTemplate
 import org.springframework.web.bind.annotation.RestController
 
@@ -20,19 +18,26 @@ class GameController(
     private val messagingTemplate: SimpMessagingTemplate
 ) {
 
+    companion object {
+        const val DEBUG_SESSION_ID = "5e34"
+    }
+
     @MessageMapping("/game")
     fun beginGame(beginGameRequest: BeginGameRequest) =
         with(beginGameRequest) {
             messagingTemplate.convertAndSend(
-                "/topic/${sessionId}/game",
+                "/topic/$sessionId/game",
                 questionService.getQuestions(sessionId)
             )
         }
 
     @MessageMapping("/gameDebug")
-    @SendTo("/topic/game")
-    fun debugGame(beginGameRequest: BeginGameDebugRequest): QuestionSetResponse =
-        questionService.getQuestionsByIdx(beginGameRequest.questionId)
+    fun debugGame(beginGameRequest: BeginGameDebugRequest) {
+        messagingTemplate.convertAndSend(
+            "/topic/$DEBUG_SESSION_ID/game",
+            questionService.getQuestionsByIdx(beginGameRequest.questionId)
+        )
+    }
 
     @MessageMapping("/solution")
     fun checkQuestion(questionSolution: QuestionSolutionRequest) =
